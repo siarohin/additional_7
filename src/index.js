9faa420c0a1d60f68c3fc7086296669ee07e5be7
A.Seregin <a.siarohin@gmail.com>
2018-10-05 21:15:44 +0300

    *

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+	
+	const arr = matrix.concat();
+	
+	for (let i = 0; i < 9; i++) {
+
+		for (let j = 0; j < 9; j++) {
+			
+			matrix[i][j] == '' ? arr[i][j] = [].concat() : '';
+
+		}
+
+	}
+
+	return arr;
+
 }
